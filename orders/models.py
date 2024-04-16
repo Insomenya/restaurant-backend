623be008a71b95d7444a0dc5ed3498245a11a122
Insomenya <kolowertnov.alexej@yandex.com>
2024-04-16 23:13:02 +0400

    first commit

---
+++ orders/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+
+class Order(models.Model):
+
+    STATUS = (
+        ('ADDED', 'Создан'),
+        ('COMPLETED', 'Завершен')
+    )
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS, default=STATUS[0][0])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"<Order {self.status} by {self.customer.id}"
